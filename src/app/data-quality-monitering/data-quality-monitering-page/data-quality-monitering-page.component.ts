3074099571d5c6d8b7fe08df1dd5cec139bcea05
Veerendra <kurraveerendrachowdary2012@gmail.com>
2017-12-08 15:49:08 +0530

    minor changes

--- src/app/data-quality-monitering/data-quality-monitering-page/data-quality-monitering-page.component.ts
+++ src/app/data-quality-monitering/data-quality-monitering-page/data-quality-monitering-page.component.ts
@@ -5,7 +5,7 @@ import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-data-quality-monitering-page',
   templateUrl: './data-quality-monitering-page.component.html',
   styleUrls: ['./data-quality-monitering-page.component.css'],
-  providers: [ NgbDropdownConfig ]
+  providers: [NgbDropdownConfig]
 })
 export class DataQualityMoniteringPageComponent implements OnInit {
   drop1: String;
@@ -19,9 +19,105 @@ export class DataQualityMoniteringPageComponent implements OnInit {
     this.drop3 = 'All';
     this.drop4 = 'All';
     this.drop5 = 'ADS';
-   }
-
-  ngOnInit() {
   }
-
+  grid1config = {
+    type: 'bar',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: '0',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          stack: 'Stack 0',
+          backgroundColor: 'blue',
+          options: {
+            scales: {
+              xAxes: [
+                {
+                  stacked: true
+                }
+              ],
+              yAxes: [
+                {
+                  stacked: true
+                }
+              ]
+            }
+          }
+        },
+        {
+          label: '3',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          stack: 'Stack 1',
+          backgroundColor: 'green'
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  };
+  grid2config = {
+    type: 'bar',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: '0',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          stack: 'Stack 0',
+          backgroundColor: 'blue',
+          options: {
+            scales: {
+              xAxes: [
+                {
+                  stacked: true
+                }
+              ],
+              yAxes: [
+                {
+                  stacked: true
+                }
+              ]
+            }
+          }
+        },
+        {
+          label: '1',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          stack: 'Stack 0',
+          backgroundColor: 'red',
+          options: {
+            scales: {
+              xAxes: [
+                {
+                  stacked: true
+                }
+              ],
+              yAxes: [
+                {
+                  stacked: true
+                }
+              ]
+            }
+          }
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [{
+          type: 'linear',
+          position: 'left',
+        }, {
+          type: 'linear',
+          position: 'right',
+        }]
+      }
+    }
+  };
+  ngOnInit() {}
 }
