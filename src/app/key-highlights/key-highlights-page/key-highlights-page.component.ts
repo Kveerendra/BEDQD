ce26082663165f15202f79ac302084f012c11853
Kurra Veerendra Chowdary <kurraveerendrachowdary2012@gmail.com>
2018-04-10 20:39:45 +0530

    modified key heighlights

--- src/app/key-highlights/key-highlights-page/key-highlights-page.component.ts
+++ src/app/key-highlights/key-highlights-page/key-highlights-page.component.ts
@@ -1,12 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-key-highlights-page',
   templateUrl: './key-highlights-page.component.html',
-  styleUrls: ['./key-highlights-page.component.css']
+  styleUrls: ['./key-highlights-page.component.css'],
+  providers: [NgbDropdownConfig]
 })
 export class KeyHighlightsPageComponent implements OnInit {
-
+  grid1config = {
+    type: 'pie',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+  };
+  grid2config = {
+    type: 'pie',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+  };
   constructor() { }
 
   ngOnInit() {
--- src/app/key-highlights/key-highlights.module.ts
+++ src/app/key-highlights/key-highlights.module.ts
@@ -4,12 +4,14 @@ import { DqScoreComponent } from './dq-score/dq-score.component';
 import { KeyHighlightsPageComponent } from './key-highlights-page/key-highlights-page.component';
 import { ChartModule } from 'angular2-chartjs';
 import { SharedModule } from '../shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     ChartModule,
-    SharedModule
+    SharedModule,
+    NgbModule.forRoot()
   ],
   declarations: [DqScoreComponent, KeyHighlightsPageComponent],
   exports: [KeyHighlightsPageComponent]
