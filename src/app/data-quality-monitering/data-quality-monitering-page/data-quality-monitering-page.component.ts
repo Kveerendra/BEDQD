18679ba9062c987e350d6aa29839d8c854afdf75
Kurra Veerendra Chowdary <kurraveerendrachowdary2012@gmail.com>
2018-04-12 12:52:03 +0530

    added dummy service

--- src/app/data-quality-monitering/data-quality-monitering-page/data-quality-monitering-page.component.ts
+++ src/app/data-quality-monitering/data-quality-monitering-page/data-quality-monitering-page.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
+import { DataQualityMoniteringService } from '../service/data-quality-monitering.service';
 
 @Component({
   selector: 'app-data-quality-monitering-page',
   templateUrl: './data-quality-monitering-page.component.html',
   styleUrls: ['./data-quality-monitering-page.component.css'],
-  providers: [NgbDropdownConfig]
+  providers: [NgbDropdownConfig,DataQualityMoniteringService]
 })
 export class DataQualityMoniteringPageComponent implements OnInit {
+  dataQualityScoreModel ={};
   drop1: String;
   drop2: String;
   drop3: String;
   drop4: String;
   drop5: String;
-  constructor() {
+  constructor(DataQualityMoniteringService:DataQualityMoniteringService) {
     this.drop1 = 'Source System';
     this.drop2 = 'All';
     this.drop3 = 'All';
     this.drop4 = 'All';
     this.drop5 = 'ADS';
+    this.dataQualityScoreModel = DataQualityMoniteringService.getDataQualityModel();
   }
   grid1config = {
     type: 'bar',
--- src/app/data-quality-monitering/service/data-quality-monitering.service.ts
+++ src/app/data-quality-monitering/service/data-quality-monitering.service.ts
@@ -2,7 +2,64 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataQualityMoniteringService {
-
+dataQualityJsonObject = {
+  "bcdeWithDQModel": {
+      "header": "BCDE_WITH_DQ_MONITORING",
+      "bcdeWithDQcore": "39"
+  },
+  "dQScoreModel": {
+      "header": "DATA_QUALITY_SCORE",
+      "dqScore": "83.39"
+  },
+  "ecdeWithDQModel": {
+      "header": "ECDE_DQ_MNTR",
+      "ecdeWithDQScore": "50"
+  },
+  "perstOfAdsProfileModel": [
+      {
+          "header": "% of ADS Profiled",
+          "bucfName": "CAO",
+          "adsCount": "1",
+          "prQtr": "100",
+          "crntQtr": "100"
+      },
+      {
+          "header": "% of ADS Profiled",
+          "bucfName": "QS",
+          "adsCount": "1",
+          "prQtr": "100",
+          "crntQtr": "100"
+      },
+     
+  ],
+  "dQMonitoringDetailsbySourceSystem": [
+      {
+          "databaseName": "HR GDW",
+          "ads": "Archer",
+          "bucfName": "CD",
+          "dataQualityScore": "99.99",
+          "chgFrmPrQtr": "50.03",
+          "rcrdsPsd": "5458.0",
+          "rowsTstd": "5459.0",
+          "header": "DQ_MONITORING_BY_SOURCE_SYSTEM"
+      },
+     
+  ],
+  "eCDEandBCDEwithDQmonitoringbyADS": [
+      {
+          "ads": "Insurance ...",
+          "bucfName": "CD",
+          "yearQtr": "Nov-Dec 2010",
+          "bcdeTot": "1",
+          "ecdeTot": "1",
+          "header": "ECDE_AND_BCDE_WITH_DQ_MONITORING_BY_ADS"
+      }
+  ]
+};
   constructor() { }
+
+  getDataQualityModel(){
+    return this.dataQualityJsonObject.dQScoreModel;
+  }
 
 }
