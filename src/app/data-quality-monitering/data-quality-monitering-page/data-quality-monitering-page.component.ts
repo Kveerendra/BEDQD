4b11494a561a80c877aefadc19b1f05a77774973
Kurra Veerendra Chowdary <kurraveerendrachowdary2012@gmail.com>
2018-04-26 21:45:43 +0530

    fixed tslint issue

--- src/app/data-quality-monitering/data-quality-monitering-page/data-quality-monitering-page.component.ts
+++ src/app/data-quality-monitering/data-quality-monitering-page/data-quality-monitering-page.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, Inject,ViewChild } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 import { DataQualityMoniteringService } from '../service/data-quality-monitering.service';
-import {ChangeDetectorRef} from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 import { ChartComponent } from 'angular2-chartjs';
-
 
 @Component({
   selector: 'app-data-quality-monitering-page',
@@ -11,12 +10,9 @@ import { ChartComponent } from 'angular2-chartjs';
   styleUrls: ['./data-quality-monitering-page.component.css'],
   providers: [NgbDropdownConfig, DataQualityMoniteringService]
 })
-
 export class DataQualityMoniteringPageComponent implements OnInit {
-  @ViewChild("grid1Chart")
-  chart1:ChartComponent;
-  @ViewChild("grid2Chart")
-  chart2:ChartComponent;
+  @ViewChild('grid1Chart') chart1: ChartComponent;
+  @ViewChild('grid2Chart') chart2: ChartComponent;
   LOBFilter = {};
   SourceSysFilter = {};
   QuarterFilterQtr = {};
@@ -39,7 +35,7 @@ export class DataQualityMoniteringPageComponent implements OnInit {
           label: 'Current Quarter',
           data: [],
           stack: 'Stack 0',
-          backgroundColor: '#007acc',
+          backgroundColor: '#007acc'
         },
         {
           label: 'Prior Quarter',
@@ -52,34 +48,45 @@ export class DataQualityMoniteringPageComponent implements OnInit {
     options: {
       responsive: true,
       maintainAspectRatio: false,
-    legend:{
-    display:true
-  },
-  tooltips:{
-    enabled:true,
-    mode :'index'
-  },
-  scales: {
-        xAxes: [{
+      legend: {
+        display: true
+      },
+      tooltips: {
+        enabled: true,
+        mode: 'index'
+      },
+      scales: {
+        xAxes: [
+          {
             barPercentage: 0.8
-        }],
-        yAxes: [{
-           scaleLabel: {
-        display: true,
-        labelString: '% Profiled'
-      } 
-        }],
+          }
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: '% Profiled'
+            }
+          }
+        ]
+      }
     }
-  }
-
   };
   grid2config = {
     type: 'bar',
     data: {
-      labels: ['Archer', 'BluePrint', 'CREST', 'Finance FW', 'GOALD-UK', 'Insurence...', 'Service No...'],
+      labels: [
+        'Archer',
+        'BluePrint',
+        'CREST',
+        'Finance FW',
+        'GOALD-UK',
+        'Insurence...',
+        'Service No...'
+      ],
       datasets: [
         {
-        label : '',
+          label: '',
           data: [],
           stack: 'Stack 0',
           backgroundColor: '#29a329',
@@ -93,9 +100,9 @@ export class DataQualityMoniteringPageComponent implements OnInit {
               yAxes: [
                 {
                   scaleLabel: {
-                  display: true,
-                 labelString: '% Profiled'
-               },
+                    display: true,
+                    labelString: '% Profiled'
+                  },
                   stacked: true
                 }
               ]
@@ -103,81 +110,117 @@ export class DataQualityMoniteringPageComponent implements OnInit {
           }
         },
         {
-        label : '',
+          label: '',
           data: [],
           stack: 'Stack 0',
           backgroundColor: '#007acc'
         }
       ]
     },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: true
+      },
+      tooltips: {
+        enabled: true,
+        mode: 'index'
+      },
+      scales: {
+        xAxes: [
+          {
+            barPercentage: 0.8
+          }
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: '% Profiled'
+            }
+          }
+        ]
+      }
+    }
   };
   service: DataQualityMoniteringService;
-  constructor(private dataQualityMoniteringService: DataQualityMoniteringService,private cd : ChangeDetectorRef) {
+  constructor(
+    private dataQualityMoniteringService: DataQualityMoniteringService,
+    private cd: ChangeDetectorRef
+  ) {
     this.drop1 = 'Source System';
     this.drop5 = 'ADS';
     this.service = dataQualityMoniteringService;
   }
 
   ngOnInit() {
-    
-    this.service.getData().then((dataaa) => {
-     
+    this.service.getData().then(dataaa => {
       this.dataQualityScoreModel = this.service.getdQScoreModel();
       this.bcdeWithDQModel = this.service.getbcdeWithDQModel();
       this.ecdeWithDQModel = this.service.getecdeWithDQModel();
-      var recievedData_1 = this.service.getperstOfAdsProfileModel();
-      var receivedData_2 = this.service.geteCDEandBCDEwithDQmonitoringbyADS();
+      const recievedData_1 = this.service.getperstOfAdsProfileModel();
+      const receivedData_2 = this.service.geteCDEandBCDEwithDQmonitoringbyADS();
 
-      var dropDown2 = this.service.getdQMonitoringDetailsbySourceSystem();
-      for(var k in dropDown2){
-        if(this.drop2.indexOf(dropDown2[k]["ads"]) == -1){
-          this.drop2.push(dropDown2[k]["ads"]);
-          this.SourceSysFilter[dropDown2[k]["ads"]] = true;
+      const dropDown2 = this.service.getdQMonitoringDetailsbySourceSystem();
+      for (const k in dropDown2) {
+        if (this.drop2.indexOf(dropDown2[k]['ads']) === -1) {
+          this.drop2.push(dropDown2[k]['ads']);
+          this.SourceSysFilter[dropDown2[k]['ads']] = true;
         }
       }
-     for(var j in recievedData_1){
-      if(this.drop4.indexOf(recievedData_1[j]["bucfName"]) == -1){
-         this.drop4.push(recievedData_1[j]["bucfName"]);
-         this.LOBFilter[recievedData_1[j]["bucfName"]] = true;
+      for (const j in recievedData_1) {
+        if (this.drop4.indexOf(recievedData_1[j]['bucfName']) === -1) {
+          this.drop4.push(recievedData_1[j]['bucfName']);
+          this.LOBFilter[recievedData_1[j]['bucfName']] = true;
         }
-        
       }
       this.grid1config.data.labels = this.drop4;
-     for(var l in receivedData_2){
-      if(this.drop3.indexOf(receivedData_2[l]["yearQtr"]) == -1){
-         this.drop3.push(receivedData_2[l]["yearQtr"]);
-         this.QuarterFilterQtr[receivedData_2[l]["yearQtr"]] = true;
+      for (const l in receivedData_2) {
+        if (this.drop3.indexOf(receivedData_2[l]['yearQtr']) === -1) {
+          this.drop3.push(receivedData_2[l]['yearQtr']);
+          this.QuarterFilterQtr[receivedData_2[l]['yearQtr']] = true;
         }
-        
       }
-      //alert(this.drop4);
-      var data = {
+      // alert(this.drop4);
+      const data = {
         label: '0',
         data: [],
         stack: 'Stack 0',
         backgroundColor: 'blue'
       };
-      var priorQrtr = this.grid1config.data.datasets[0].data;
-      var currentQrtr = this.grid1config.data.datasets[1].data;
+      const priorQrtr = this.grid1config.data.datasets[0].data;
+      const currentQrtr = this.grid1config.data.datasets[1].data;
 
-      var priorQrtr_2 = this.grid2config.data.datasets[0].data;
-      var currentQrtr_2 = this.grid2config.data.datasets[1].data;
+      const priorQrtr_2 = this.grid2config.data.datasets[0].data;
+      const currentQrtr_2 = this.grid2config.data.datasets[1].data;
 
-      for (var i in recievedData_1) {
-        this.grid1config.data.datasets[0].data.push(recievedData_1[i]["prQtr"]);
-        this.grid1config.data.datasets[1].data.push(recievedData_1[i]["crntQtr"]);
+      for (const i in recievedData_1) {
+        if (recievedData_1[i]) {
+          this.grid1config.data.datasets[0].data.push(
+            recievedData_1[i]['prQtr']
+          );
+          this.grid1config.data.datasets[1].data.push(
+            recievedData_1[i]['crntQtr']
+          );
+        }
       }
-     
+
       this.grid1loaded = true;
 
-      for (var j in receivedData_2) {
-        this.grid2config.data.datasets[0].data.push(receivedData_2[j]["bcdeTot"]);
-        this.grid2config.data.datasets[1].data.push(receivedData_2[j]["ecdeTot"]);
+      for (const j in receivedData_2) {
+        if (receivedData_2[j]) {
+          this.grid2config.data.datasets[0].data.push(
+            receivedData_2[j]['bcdeTot']
+          );
+          this.grid2config.data.datasets[1].data.push(
+            receivedData_2[j]['ecdeTot']
+          );
+        }
       }
       this.grid2loaded = true;
-      
 
-    /*  datasets = [
+      /*  datasets = [
         {
           label: '0',
           data: priorQrtr,
@@ -191,52 +234,51 @@ export class DataQualityMoniteringPageComponent implements OnInit {
           backgroundColor: 'green'
         }
       ]*/
-      //this.grid1config.data.datasets = datasets;
+      // this.grid1config.data.datasets = datasets;
     });
- 
   }
-  
-filterData(e){
-  var labels = [];
-for(var key in this.LOBFilter){
-  if(this.LOBFilter[key]){
-    labels.push(key);
+
+  filterData(e) {
+    const labels = [];
+    for (const key in this.LOBFilter) {
+      if (this.LOBFilter[key]) {
+        labels.push(key);
+      }
+    }
+    this.chart1.data.labels = labels;
+    this.chart1.chart.update();
+  }
+
+  filterSourceSystemData(e) {
+    const labels = [];
+    for (const key in this.SourceSysFilter) {
+      if (this.SourceSysFilter[key]) {
+        labels.push(key);
+      }
+    }
+    this.chart2.data.labels = labels;
+
+    this.chart2.chart.update();
+  }
+
+  filterQuarterData(e) {
+    this.service.getData().then(dataaa => {
+      const quarterlabels = this.service.geteCDEandBCDEwithDQmonitoringbyADS();
+      this.grid2config.data.datasets[0].data = [];
+      this.grid2config.data.datasets[1].data = [];
+      for (const i in quarterlabels) {
+        if (this.QuarterFilterQtr[quarterlabels[i]['yearQtr']]) {
+          this.grid2config.data.datasets[0].data.push(
+            quarterlabels[i]['bcdeTot']
+          );
+          this.grid2config.data.datasets[1].data.push(
+            quarterlabels[i]['ecdeTot']
+          );
+        }
+      }
+      this.chart2.data.datasets[0].data = this.grid2config.data.datasets[0].data;
+      this.chart2.data.datasets[1].data = this.grid2config.data.datasets[1].data;
+      this.chart2.chart.update();
+    });
   }
 }
-this.chart1.data.labels =labels;
-this.chart1.chart.update();
-}
-
-filterSourceSystemData(e){
-  var labels = [];
-for(var key in this.SourceSysFilter){
-  if(this.SourceSysFilter[key]){
-    labels.push(key);
-  }
-}
-this.chart2.data.labels =labels;
-
-this.chart2.chart.update();
-}
-
-filterQuarterData(e){
-this.service.getData().then((dataaa) => {
-var quarterlabels = this.service.geteCDEandBCDEwithDQmonitoringbyADS();
-this.grid2config.data.datasets[0].data = [];
-this.grid2config.data.datasets[1].data = [];
-for(var i in quarterlabels){
-  if(this.QuarterFilterQtr[quarterlabels[i]["yearQtr"]]){
-    this.grid2config.data.datasets[0].data.push(quarterlabels[i]["bcdeTot"]);
-    this.grid2config.data.datasets[1].data.push(quarterlabels[i]["ecdeTot"]);
-  }
-}
-     this.chart2.data.datasets[0].data = this.grid2config.data.datasets[0].data;
-this.chart2.data.datasets[1].data = this.grid2config.data.datasets[1].data;
-this.chart2.chart.update();
- });
-  }
-  
-
-
-}
-
