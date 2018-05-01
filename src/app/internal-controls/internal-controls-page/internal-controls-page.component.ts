37707167fa3c4372228c7839490c47c7c6250855
Kurra Veerendra Chowdary <kurraveerendrachowdary2012@gmail.com>
2018-05-02 00:26:58 +0530

    latest modifications

--- src/app/internal-controls/internal-controls-page/internal-controls-page.component.ts
+++ src/app/internal-controls/internal-controls-page/internal-controls-page.component.ts
@@ -8,180 +8,28 @@ import { InternalControlService } from '../service/internal-control.service';
 })
 export class InternalControlsPageComponent implements OnInit {
 
-  grid1loaded = false;
-  grid2loaded = false;
   impactScoreModel = {};
   dQPScoreModel = {};
   dqRIScoreModel = {};
-  drop1: String;
-  drop2: String[] = [];
-  drop3: String[] = [];
-  drop4: String[] = [];
-  drop5: String;
-  grid1config = {
-    type: 'bar',
-    data: {
-      labels: ['CAO', 'CD', 'HR', 'Legal', 'QS', 'XXXGS', 'XYZ'],
-      datasets: [
-        {
-          label: 'Current Quarter',
-          data: [],
-          stack: 'Stack 0',
-          backgroundColor: '#007acc',
-        },
-        {
-          label: 'Prior Quarter',
-          data: [],
-          stack: 'Stack 1',
-          backgroundColor: '#29a329'
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-    legend:{
-    display:true
-  },
-  tooltips:{
-    enabled:true,
-    mode :'index'
-  },
-  scales: {
-        xAxes: [{
-            barPercentage: 0.8
-        }],
-        yAxes: [{
-           scaleLabel: {
-        display: true,
-        labelString: '% Profiled'
-      } 
-        }],
-    }
-  }
-
-  };
-  grid2config = {
-    type: 'bar',
-    data: {
-      labels: ['Archer', 'BluePrint', 'CREST', 'Finance FW', 'GOALD-UK', 'Insurence...', 'Service No...'],
-      datasets: [
-        {
-        label : '',
-          data: [],
-          stack: 'Stack 0',
-          backgroundColor: '#29a329',
-          options: {
-            scales: {
-              xAxes: [
-                {
-                  stacked: true
-                }
-              ],
-              yAxes: [
-                {
-                  scaleLabel: {
-                  display: true,
-                 labelString: '% Profiled'
-               },
-                  stacked: true
-                }
-              ]
-            }
-          }
-        },
-        {
-        label : '',
-          data: [],
-          stack: 'Stack 0',
-          backgroundColor: '#007acc'
-        }
-      ]
-    },
-  };
+  LOBFilter = {};
+  sourceSystemFilter = {};
   service: InternalControlService;
   constructor(internalControlService: InternalControlService) {
-    this.drop1 = 'Source System';
-    this.drop5 = 'ADS';
     this.service = internalControlService;
   }
 
   ngOnInit() {
-    
+
     this.service.getData().then((dataaa) => {
-     
+
       this.dQPScoreModel = this.service.getQPModel();
       this.impactScoreModel = this.service.getImpactScoreModel();
       this.dqRIScoreModel = this.service.getdQScoreModel();
-      /* var recievedData_1 = this.service.getperstOfAdsProfileModel();
-      var receivedData_2 = this.service.geteCDEandBCDEwithDQmonitoringbyADS();
 
-      var dropDown2 = this.service.getdQMonitoringDetailsbySourceSystem();
-      for(var k in dropDown2){
-        if(this.drop2.indexOf(dropDown2[k]["ads"]) == -1){
-          this.drop2.push(dropDown2[k]["ads"]);
-        }
-      }
-     for(var j in recievedData_1){
-      if(this.drop4.indexOf(recievedData_1[j]["bucfName"]) == -1){
-         this.drop4.push(recievedData_1[j]["bucfName"]);
-        }
-        
-      }
-     for(var l in receivedData_2){
-      if(this.drop3.indexOf(receivedData_2[l]["yearQtr"]) == -1){
-         this.drop3.push(receivedData_2[l]["yearQtr"]);
-        }
-        
-      }
-      //alert(this.drop4);
-      var data = {
-        label: '0',
-        data: [],
-        stack: 'Stack 0',
-        backgroundColor: 'blue'
-      };
-      var priorQrtr = this.grid1config.data.datasets[0].data;
-      var currentQrtr = this.grid1config.data.datasets[1].data;
-
-      var priorQrtr_2 = this.grid2config.data.datasets[0].data;
-      var currentQrtr_2 = this.grid2config.data.datasets[1].data;
-
-      for (var i in recievedData_1) {
-        this.grid1config.data.datasets[0].data.push(recievedData_1[i]["prQtr"]);
-        this.grid1config.data.datasets[1].data.push(recievedData_1[i]["crntQtr"]);
-      }
-     
-      this.grid1loaded = true;
-
-      for (var j in receivedData_2) {
-        this.grid2config.data.datasets[0].data.push(receivedData_2[j]["bcdeTot"]);
-        this.grid2config.data.datasets[1].data.push(receivedData_2[j]["ecdeTot"]);
-      }
-      this.grid2loaded = true; */
-      
-
-    /*  datasets = [
-        {
-          label: '0',
-          data: priorQrtr,
-          stack: 'Stack 0',
-          backgroundColor: 'blue'
-        },
-        {
-          label: '3',
-          data: currentQrtr,
-          stack: 'Stack 1',
-          backgroundColor: 'green'
-        }
-      ]*/
-      //this.grid1config.data.datasets = datasets;
     });
- 
+
   }
-filterData(e){
-  if(!e.target.checked){  
-    alert("not checked");
-  }
+filterData(e) {
+
 }
 }
--- src/app/internal-controls/service/internal-control.service.ts
+++ src/app/internal-controls/service/internal-control.service.ts
@@ -7,25 +7,30 @@ export class InternalControlService {
   // just change this url to point to the service
   private url = 'InternalControls.json';
   constructor(private httpc: HttpClient) {
-      this.getData().then((data) => {
-          this.internalControlsJsonObject = data;
-      });
-        }
+    this.getData().then(data => {
+      this.internalControlsJsonObject = data;
+    });
+  }
   getData() {
-      console.log("inside service Internal Controls")
-      return new Promise(resolve => {
-          this.httpc.get(this.url).subscribe((data) => { resolve(data); this.internalControlsJsonObject = data; }, err => { console.error(err); });
-      });
+    return new Promise(resolve => {
+      this.httpc.get(this.url).subscribe(
+        data => {
+          resolve(data);
+          this.internalControlsJsonObject = data;
+        },
+        err => {
+          console.error(err);
+        }
+      );
+    });
   }
   getImpactScoreModel() {
-
-      return this.internalControlsJsonObject['impactScoreModel'];
+    return this.internalControlsJsonObject['impactScoreModel'];
   }
   getdQScoreModel() {
-      return this.internalControlsJsonObject['dQRIScoreModel'];
+    return this.internalControlsJsonObject['dQRIScoreModel'];
   }
   getQPModel() {
-      return this.internalControlsJsonObject['dQPScoreModel'];
+    return this.internalControlsJsonObject['dQPScoreModel'];
   }
-
 }
